a626e59b72c8b5f136c01de49c1a9c028fa9af71
Bui Thanh Long <buithanhlong1912@gmail.com>
2025-02-11 03:08:11 +0700

    update audio autoplay and modify the icon based on the audio is played or not

--- js2/main-wedding.js
+++ js2/main-wedding.js
@@ -1,39 +1,39 @@
 // AOS
 
-   //AOS ANIMATION
-   AOS.init();
+//AOS ANIMATION
+AOS.init();
 // SCROLLREVEA
 var swiper1 = new Swiper(".album-slide", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    autoplay: {
- delay: 3000,
-},
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 3000,
+  },
 });
 
+// FANCY BOX
+Fancybox.bind("[data-fancybox]", {});
 
- // FANCY BOX
- Fancybox.bind("[data-fancybox]", {
-
- });
- 
 //AUDIO
-$(".toggleAudio").on("click",function(){
-  $(this).find("i").toggleClass("ri-volume-up-fill")
-  audio = $("#audio")
-  audio[0].paused ? 
-  audio[0].play()
-  : 
-  audio[0].pause();
-  })
-
-
+$(".toggleAudio").on("click", function () {
+  let audio = $("#audio")[0];
+  let icon = $(this).find("i");
+  if (audio.paused) {
+    icon.removeClass("ri-volume-mute-fill");
+    icon.toggleClass("ri-volume-up-fill");
+    audio.play();
+  } else {
+    icon.removeClass("ri-volume-up-fill");
+    icon.toggleClass("ri-volume-mute-fill");
+    audio.pause();
+  }
+});
